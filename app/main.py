1fb1ab109662e4c4178013ca3870f8962170a9c3
hp <engrziaurrehman.kicsit@gmail.com>
2023-09-04 11:07:45 +0500

    updated

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def Predistions(inputData):
 df = userInputFeatures()
 
 GIFImage = 'https://1.bp.blogspot.com/-nUNmjUY0kwo/XIGaggwn58I/AAAAAAAAD4A/tA3S0Tgu5N4rbjlaNEbr_I5GuHbQwHyRgCEwYBhgL/s1600/image6.gif'
-    st.sidebar.image(GIFImage, caption="Face-Mesh", use_column_width=True)
+st.sidebar.image(GIFImage, caption="Face-Mesh", use_column_width=True)
 
 st.subheader('User Input parameters')
 st.write(df)
